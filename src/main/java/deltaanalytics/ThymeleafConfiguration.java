b89fe2a3209dd70cf3968fb227e3c1776fd501a9
markus <mlachmann@labor-lademannbogen.de>
2016-04-01 14:02:07 +0200

    beta

--- src/main/java/deltaanalytics/ThymeleafConfiguration.java
+++ src/main/java/deltaanalytics/ThymeleafConfiguration.java
@@ -1,11 +1,16 @@
 package deltaanalytics;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.FileTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
@@ -36,5 +41,14 @@ public class ThymeleafConfiguration {
         templateEngine.setTemplateResolver(defaultTemplateResolver());
         templateEngine.addDialect(new LayoutDialect());
         return templateEngine;
+    }
+
+    @Bean(name = "OBJECT_MAPPER_BEAN")
+    public ObjectMapper jsonObjectMapper() {
+        return Jackson2ObjectMapperBuilder.json()
+                .serializationInclusion(JsonInclude.Include.NON_NULL) // Don’t include null values
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS) //ISODate
+                .modules(new JSR310Module())
+                .build();
     }
 }
