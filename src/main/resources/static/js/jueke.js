45373ba454a7714a9ae79659acc48deea54a0694
markus <m.lachmann@labor-lademannbogen.de>
2016-04-22 14:24:17 +0200

    jueke status gui

--- src/main/resources/static/js/jueke.js
+++ src/main/resources/static/js/jueke.js
@@ -10,36 +10,68 @@ function getJuekeState() {
 
 function updateGui(json) {
     for (var i = 1; i <= 8; i++) {
-        $('#valve' + i + 'State').removeClass('active');
+        $('#valve' + i + 'State').removeClass('glyphicon glyphicon-ok-circle');
+        $('#valve' + i + 'State').removeClass('glyphicon glyphicon-ban-circle');
     }
     if (json.valveStatus1 === true) {
-        $('#valve1State').addClass('active');
+        $('#valve1State').addClass('glyphicon glyphicon-ok-circle');
+    }
+    else{
+        $('#valve1State').addClass('glyphicon glyphicon-ban-circle');
     }
     if (json.valveStatus2 === true) {
-        $('#valve2State').addClass('active');
+        $('#valve2State').addClass('glyphicon glyphicon-ok-circle');
+    }
+    else{
+        $('#valve2State').addClass('glyphicon glyphicon-ban-circle');
     }
     if (json.valveStatus3 === true) {
-        $('#valve3State').addClass('active');
+        $('#valve3State').addClass('glyphicon glyphicon-ok-circle');
+    }
+    else{
+        $('#valve3State').addClass('glyphicon glyphicon-ban-circle');
     }
     if (json.valveStatus4 === true) {
-        $('#valve4State').addClass('active');
+        $('#valve4State').addClass('glyphicon glyphicon-ok-circle');
+    }
+    else{
+        $('#valve4State').addClass('glyphicon glyphicon-ban-circle');
     }
     if (json.valveStatus5 === true) {
-        $('#valve5State').addClass('active');
+        $('#valve5State').addClass('glyphicon glyphicon-ok-circle');
+    }
+    else{
+        $('#valve5State').addClass('glyphicon glyphicon-ban-circle');
     }
     if (json.valveStatus6 === true) {
-        $('#valve6State').addClass('active');
+        $('#valve6State').addClass('glyphicon glyphicon-ok-circle');
+    }
+    else{
+        $('#valve6State').addClass('glyphicon glyphicon-ban-circle');
     }
     if (json.valveStatus7 === true) {
-        $('#valve7State').addClass('active');
+        $('#valve7State').addClass('glyphicon glyphicon-ok-circle');;
+    }
+    else{
+        $('#valve17State').addClass('glyphicon glyphicon-ban-circle');
     }
     if (json.valveStatus8 === true) {
-        $('#valve8State').addClass('active');
+        $('#valve8State').addClass('glyphicon glyphicon-ok-circle');
     }
-    $('#pumpState').val(json.statusOfPump);
-    $('#pumpSpeedState').val(json.pumpPower);
-    $('#pressureState').val(json.actualPressureCell);
-    $('#temperatureState').val(json.actualTempHeater);
+    else{
+        $('#valve8State').addClass('glyphicon glyphicon-ban-circle');
+    }
+    $('#pumpState').removeClass('glyphicon glyphicon-ok-circle');
+    $('#pumpState').removeClass('glyphicon glyphicon-ban-circle');
+    if(json.statusOfPump === true){
+        $('#pumpState').addClass('glyphicon glyphicon-ok-circle');
+    }
+    else{
+        $('#pumpState').addClass('glyphicon glyphicon-ban-circle');
+    }
+    $('#pumpSpeedState').text(json.pumpPower);
+    $('#pressureState').text(json.actualPressureCell);
+    $('#temperatureState').text(json.actualTempHeater);
 }
 
 $(function () {
